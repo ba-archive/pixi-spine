088d9f1f0415429f57336dd467d40a8af97d501f
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2024-11-02 02:28:10 +0900

    Update atlasLoader.ts

--- packages/loader-base/src/atlasLoader.ts
+++ packages/loader-base/src/atlasLoader.ts
@@ -1,11 +1,27 @@
-import { TextureAtlas } from '@pixi-spine/base';
-import { type AssetExtension, LoaderParserPriority, ResolvedAsset, Loader, checkExtension } from '@pixi/assets';
-import { BaseTexture, extensions, ExtensionType, settings, Texture, utils } from '@pixi/core';
-import type { ISpineMetadata } from './SpineLoaderAbstract';
+import { TextureAtlas } from "@pixi-spine/base";
+import {
+    type AssetExtension,
+    LoaderParserPriority,
+    ResolvedAsset,
+    Loader,
+    checkExtension,
+} from "@pixi/assets";
+import {
+    BaseTexture,
+    extensions,
+    ExtensionType,
+    settings,
+    Texture,
+    utils,
+} from "@pixi/core";
+import type { ISpineMetadata } from "./SpineLoaderAbstract";
 
 type RawAtlas = string;
 
-export const spineTextureAtlasLoader: AssetExtension<RawAtlas | TextureAtlas, ISpineMetadata> = {
+const spineTextureAtlasLoader: AssetExtension<
+    RawAtlas | TextureAtlas,
+    ISpineMetadata
+> = {
     extension: ExtensionType.Asset,
 
     // cache: {
@@ -17,11 +33,11 @@ export const spineTextureAtlasLoader: AssetExtension<RawAtlas | TextureAtlas, IS
         extension: {
             type: ExtensionType.LoadParser,
             priority: LoaderParserPriority.Normal,
-            name: 'pixi-spine-atlas-loader',
+            name: "pixi-spine-atlas-loader",
         },
 
         test(url: string): boolean {
-            return checkExtension(url, '.atlas');
+            return checkExtension(url, ".atlas");
         },
 
         async load(url: string): Promise<RawAtlas> {
@@ -33,18 +49,22 @@ export const spineTextureAtlasLoader: AssetExtension<RawAtlas | TextureAtlas, IS
         },
 
         testParse(asset: unknown, options: ResolvedAsset): Promise<boolean> {
-            const isExtensionRight = checkExtension(options.src, '.atlas');
-            const isString = typeof asset === 'string';
+            const isExtensionRight = checkExtension(options.src, ".atlas");
+            const isString = typeof asset === "string";
 
             return Promise.resolve(isExtensionRight && isString);
         },
 
-        async parse(asset: RawAtlas, options: ResolvedAsset, loader: Loader): Promise<TextureAtlas> {
+        async parse(
+            asset: RawAtlas,
+            options: ResolvedAsset,
+            loader: Loader,
+        ): Promise<TextureAtlas> {
             const metadata: ISpineMetadata = options.data;
             let basePath = utils.path.dirname(options.src);
 
-            if (basePath && basePath.lastIndexOf('/') !== basePath.length - 1) {
-                basePath += '/';
+            if (basePath && basePath.lastIndexOf("/") !== basePath.length - 1) {
+                basePath += "/";
             }
 
             let resolve = null;
@@ -58,7 +78,9 @@ export const spineTextureAtlasLoader: AssetExtension<RawAtlas | TextureAtlas, IS
             let retval;
             const resolveCallback = (newAtlas: TextureAtlas): void => {
                 if (!newAtlas) {
-                    reject('Something went terribly wrong loading a spine .atlas file\nMost likely your texture failed to load.');
+                    reject(
+                        "Something went terribly wrong loading a spine .atlas file\nMost likely your texture failed to load.",
+                    );
                 }
                 resolve(retval);
             };
@@ -66,21 +88,33 @@ export const spineTextureAtlasLoader: AssetExtension<RawAtlas | TextureAtlas, IS
             // if we have an already loaded pixi image in the image field, use that.
             if (metadata.image || metadata.images) {
                 // merge the objects
-                const pages = Object.assign(metadata.image ? { default: metadata.image } : {}, metadata.images);
+                const pages = Object.assign(
+                    metadata.image ? { default: metadata.image } : {},
+                    metadata.images,
+                );
 
                 retval = new TextureAtlas(
                     asset as RawAtlas,
                     (line: any, callback: any) => {
                         const page = pages[line] || (pages.default as any);
 
-                        if (page && page.baseTexture) callback(page.baseTexture);
+                        if (page && page.baseTexture)
+                            callback(page.baseTexture);
                         else callback(page);
                     },
-                    resolveCallback
+                    resolveCallback,
                 );
             } else {
                 // We don't have ready to use pixi textures, we need to load them now!
-                retval = new TextureAtlas(asset as RawAtlas, makeSpineTextureAtlasLoaderFunctionFromPixiLoaderObject(loader, basePath, metadata.imageMetadata), resolveCallback);
+                retval = new TextureAtlas(
+                    asset as RawAtlas,
+                    makeSpineTextureAtlasLoaderFunctionFromPixiLoaderObject(
+                        loader,
+                        basePath,
+                        metadata.imageMetadata,
+                    ),
+                    resolveCallback,
+                );
             }
 
             return (await retPromise) as TextureAtlas;
@@ -96,13 +130,27 @@ export const spineTextureAtlasLoader: AssetExtension<RawAtlas | TextureAtlas, IS
  * Ugly function to promisify the spine texture atlas loader function.
  * @public
  */
-export const makeSpineTextureAtlasLoaderFunctionFromPixiLoaderObject = (loader: Loader, atlasBasePath: string, imageMetadata: any) => {
-    return async (pageName: string, textureLoadedCallback: (tex: BaseTexture) => any): Promise<void> => {
+export const makeSpineTextureAtlasLoaderFunctionFromPixiLoaderObject = (
+    loader: Loader,
+    atlasBasePath: string,
+    imageMetadata: any,
+) => {
+    return async (
+        pageName: string,
+        textureLoadedCallback: (tex: BaseTexture) => any,
+    ): Promise<void> => {
         // const url = utils.path.join(...atlasBasePath.split(utils.path.sep), pageName); // Broken in upstream
 
-        const url = utils.path.normalize([...atlasBasePath.split(utils.path.sep), pageName].join(utils.path.sep));
+        const url = utils.path.normalize(
+            [...atlasBasePath.split(utils.path.sep), pageName].join(
+                utils.path.sep,
+            ),
+        );
 
-        const texture = await loader.load<Texture>({ src: url, data: imageMetadata });
+        const texture = await loader.load<Texture>({
+            src: url,
+            data: imageMetadata,
+        });
 
         textureLoadedCallback(texture.baseTexture);
     };
